b2eca33216cf98f7028f53f8214e154c1d3e94f9
phanthaibao <phanbao2648@gmail.com>
2025-12-14 21:58:00 +0700

    Implement basic form validation logic

--- form-validation/validator.js
+++ form-validation/validator.js
@@ -1,14 +1,44 @@
 function Validator(options) {
     var formElement = document.querySelector(options.form)
     if (formElement) {
-        console.log(formElement)
+        options.rules.forEach(rule => {
+            var inputElement = formElement.querySelector(rule.selector)
+            var errorElement = inputElement.parentElement.querySelector('.form-message')
+            if (inputElement) {
+                inputElement.onblur = () => {
+                    var errorMessage = rule.test(inputElement.value)
+
+                    if (errorMessage) {
+                        errorElement.innerText = errorMessage
+                        inputElement.parentElement.classList.add('invalid')
+                    } else {
+                       errorElement.innerText = "" 
+                       inputElement.parentElement.classList.remove('invalid')
+                    }
+                }
+            }
+        });
     }
 }
 
-Validator.isRequired = function() {
-
+// định nghĩa rules
+//nguyên tắc rule:
+// 1. khi có lỗi => message error
+// 2. khi ko có lỗi => undefined
+Validator.isRequired = function(selector) {
+    return {
+        selector: selector,
+        test: (value) => {
+            return value.trim() ? undefined : 'vui lòng nhập đủ trường này'
+        }
+    }
 }
 
-Validator.isEmail = function() {
-
+Validator.isEmail = function(selector) {
+    return {
+        selector: selector,
+        test: () => {
+            
+        }
+    }
 }
